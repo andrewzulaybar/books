a64a4475ce862525a0a71a6e05ba9abfc869e1e8
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-05-15 23:46:45 -0700

    Add more attributes to publication table

--- api/internal/sql/init.sql
+++ api/internal/sql/init.sql
@@ -1,9 +1,75 @@
 INSERT INTO publication
-    (author, image_url, title)
+    (author, edition_pub_date, format, image_url, isbn, isbn13, language, num_pages, publisher, title)
 VALUES
-    ('Sally Rooney', 'https://images-na.ssl-images-amazon.com/images/I/81X4R7QhFkL.jpg', 'Normal People'),
-    ('Celeste Ng', 'https://images-na.ssl-images-amazon.com/images/I/91twTG-CQ8L.jpg', 'Little Fires Everywhere'),
-    ('Delia Owens', 'https://images-na.ssl-images-amazon.com/images/I/51j5p18mJNL.jpg', 'Where the Crawdads Sing'),
-    ('F. Scott Fitzgerald', 'https://images-na.ssl-images-amazon.com/images/I/81af+MCATTL.jpg', 'The Great Gatsby'),
-    ('Jeanine Cummins', 'https://images-na.ssl-images-amazon.com/images/I/81iVsj91eQL.jpg', 'American Dirt'),
-    ('James Clear', 'https://images-na.ssl-images-amazon.com/images/I/91Xq+S+F2jL.jpg', 'Atomic Habits');
+    (
+        'Sally Rooney',
+        '2019-04-16',
+        'Hardcover',
+        'https://images-na.ssl-images-amazon.com/images/I/81X4R7QhFkL.jpg',
+        '1984822179',
+        '9781984822178',
+        'English',
+        288,
+        'Hogarth',
+        'Normal People'
+    ),
+    (
+        'Celeste Ng',
+        '2017-09-12',
+        'Hardcover',
+        'https://images-na.ssl-images-amazon.com/images/I/91twTG-CQ8L.jpg',
+        '0735224293',
+        '9780735224292',
+        'English',
+        352,
+        'Penguin Press',
+        'Little Fires Everywhere'
+    ),
+    (
+        'Delia Owens',
+        '2018-08-14',
+        'Hardcover',
+        'https://images-na.ssl-images-amazon.com/images/I/51j5p18mJNL.jpg',
+        '0735219095',
+        '9780735219090',
+        'English',
+        384,
+        'G.P. Putnam''s Sons',
+        'Where the Crawdads Sing'
+    ),
+    (
+        'F. Scott Fitzgerald',
+        '2004-09-30',
+        'Paperback',
+        'https://images-na.ssl-images-amazon.com/images/I/81af+MCATTL.jpg',
+        '0743273567',
+        '9780743273565',
+        'English',
+        180,
+        'Scribner',
+        'The Great Gatsby'
+    ),
+    (
+        'Jeanine Cummins',
+        '2020-01-21',
+        'Hardcover',
+        'https://images-na.ssl-images-amazon.com/images/I/81iVsj91eQL.jpg',
+        '1250209765',
+        '9781250209764',
+        'English',
+        400,
+        'Flatiron Books',
+        'American Dirt'
+    ),
+    (
+        'James Clear',
+        '2018-09-16',
+        'Hardcover',
+        'https://images-na.ssl-images-amazon.com/images/I/91Xq+S+F2jL.jpg',
+        '0735211299',
+        '9780735211292',
+        'English',
+        320,
+        'Avery',
+        'Atomic Habits'
+    );
--- api/internal/sql/schema.sql
+++ api/internal/sql/schema.sql
@@ -4,6 +4,14 @@ CREATE TABLE publication
 (
     id SERIAL PRIMARY KEY,
     author VARCHAR (100) NOT NULL,
-    image_url VARCHAR (300) NOT NULL,
-    title VARCHAR (200) NOT NULL
+    edition_pub_date DATE NOT NULL,
+    format VARCHAR (30) NOT NULL,
+    image_url VARCHAR (300) UNIQUE NOT NULL,
+    isbn VARCHAR (10) UNIQUE NOT NULL,
+    isbn13 VARCHAR (13) UNIQUE NOT NULL,
+    language VARCHAR (100) NOT NULL,
+    num_pages INTEGER NOT NULL,
+    publisher VARCHAR (100) NOT NULL,
+    title VARCHAR (200) NOT NULL,
+    UNIQUE (author, title)
 );

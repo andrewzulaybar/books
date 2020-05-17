01363723da45566e291c111549d6cdabb56cab89
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-05-17 15:17:22 -0700

    Add work table

--- api/internal/sql/schema.sql
+++ api/internal/sql/schema.sql
@@ -1,9 +1,19 @@
-DROP TABLE IF EXISTS publication;
+DROP TABLE IF EXISTS work, publication;
+
+CREATE TABLE work
+(
+    id SERIAL PRIMARY KEY,
+    author VARCHAR (100) NOT NULL,
+    description VARCHAR (5000) NOT NULL,
+    initial_pub_date DATE NOT NULL,
+    original_language VARCHAR (100) NOT NULL,
+    title VARCHAR (200) NOT NULL,
+    UNIQUE (author, title)
+);
 
 CREATE TABLE publication
 (
     id SERIAL PRIMARY KEY,
-    author VARCHAR (100) NOT NULL,
     edition_pub_date DATE NOT NULL,
     format VARCHAR (30) NOT NULL,
     image_url VARCHAR (300) UNIQUE NOT NULL,
@@ -12,6 +22,6 @@ CREATE TABLE publication
     language VARCHAR (100) NOT NULL,
     num_pages INTEGER NOT NULL,
     publisher VARCHAR (100) NOT NULL,
-    title VARCHAR (200) NOT NULL,
-    UNIQUE (author, title)
+    work_id INTEGER NOT NULL,
+    FOREIGN KEY (work_id) REFERENCES work (id)
 );
